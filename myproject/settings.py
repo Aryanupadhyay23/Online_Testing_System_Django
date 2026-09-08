00de98dec57dfb0421a708d614f4de88c0bee492
Aryanupadhyay23 <au3162233@gmail.com>
2026-09-08 15:50:46 +0530

    pushing to remote

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'learning',
         'USER': 'root',
-        'PASSWORD': 'Aryan@3012',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
